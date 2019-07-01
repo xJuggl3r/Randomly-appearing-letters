764142dddb9a08c2fd1ffc75f2b73e7028514961
xJuggl3r <50606269+xJuggl3r@users.noreply.github.com>
2019-07-01 17:31:55 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+  var nodes = document.getElementsByClassName("overlay");
+  for (var i = 0; i < nodes.length; i++) {
+    var words = nodes[i].innerText;
+    var html = "";
+    for (var i2 = 0; i2 < words.length; i2++) {
+      if (words[i2] == " ") html += words[i2];
+      else html += "<span>" + words[i2] + "</span>";
+    }
+    nodes[i].innerHTML = html;
+  }
+});
